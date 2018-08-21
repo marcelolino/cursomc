741e2678f9a0c82a7372071cfda910956a75c704
Marcelo Lino <marcelu.lino@gmail.com>
2018-08-21 15:54:38 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/nelioalves/cursomc/domain/Pedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pedido.java
@@ -2,13 +2,17 @@ package com.nelioalves.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
- import javax.persistence.CascadeType;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
  @Entity
 public class Pedido implements Serializable {
@@ -27,6 +31,9 @@ public class Pedido implements Serializable {
 	@ManyToOne
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
+	
+	@OneToMany(mappedBy="id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	public Pedido() {
 	}
@@ -67,6 +74,13 @@ public class Pedido implements Serializable {
  	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+ 	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+ 	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+ 	
  	@Override
 	public int hashCode() {
 		final int prime = 31;
